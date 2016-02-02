cd535f5a514e64201e0c18ade104b515e29e1023
Vladimir Kuznetsov <mistakster@gmail.com>
2016-02-02 11:50:48 +0500

    added plugin code

---
+++ test/index.js
@@ -0,0 +1,29 @@
+const code = `
+function decrement(a) {
+	return a - 1;
+}
+// trim-below
+/**
+ * block comment
+ */
+function square(n) {
+  // inline comment
+  foo === undefined;
+  return n * n;
+}
+`;
+
+/*
+require('babel-register')({
+	ignore: /(node_modules)/,
+	presets: ['es2015']
+});
+*/
+
+const result = require('babel-core').transform(code, {
+	plugins: [
+		'../lib/index.js'
+	]
+});
+
+console.log(result.code);
