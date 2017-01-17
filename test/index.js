f034aeb48ab6f33c145850998e111ec0252a64f2
Dmitry Nikulin <dmitry.nikulin@graph.uk>
2017-01-17 17:22:09 +0500

    some new code

--- test/index.js
+++ test/index.js
@@ -4,28 +4,25 @@ const babelCore = require('babel-core');
 describe('babel-plugin-trimmer', () => {
   it('should trim all code after "trim-below" marker', () => {
     const code = `
-function decrement(a) {
-  return a - 1;
+function Tips() {
+  return 321;
 }
+// comment-1
 // trim-below
-/**
- * block comment
- */
-function square(n) {
-  // inline comment
-  foo === undefined;
-  return n * n;
+// comment-3
+function printTips() {
+  return 123;
 }
+// comment-4
 `;
 
     const expected = `
-function decrement(a) {
-  return a - 1;
+function Tips() {
+  return 321;
 }
+// comment-1
 // trim-below
-/**
- * block comment
- */`;
+// comment-3`;
 
     const result = babelCore.transform(code, {
       plugins: [
@@ -38,51 +35,26 @@ function decrement(a) {
 
   it('should trim all code before "trim-above" marker', () => {
     const code = `
-function decrement(a) {
-  return a - 1;
+function Tips() {
+  return 321;
 }
+// comment-1
 // trim-above
-/**
- * block comment
- */
-function square(n) {
-  // inline comment
-  foo === undefined;
-  return n * n;
+// comment-3
+function printTips() {
+  return 123;
 }
+// comment-4
 `;
 
     const expected = `
+// comment-1
 // trim-above
-/**
- * block comment
- */
-function square(n) {
-  // inline comment
-  foo === undefined;
-  return n * n;
-}`;
-
-    const result = babelCore.transform(code, {
-      plugins: [
-        './lib/index.js'
-      ]
-    });
-
-    assert.strictEqual(result.code, expected);
-  });
-
-  it('should trim all code before "trim-above" marker at the bottom', () => {
-    const code = `
-function decrement(a) {
-  return a - 1;
+// comment-3
+function printTips() {
+  return 123;
 }
-// trim-above
-`;
-
-    const expected = `
-// trim-above
-`;
+// comment-4`;
 
     const result = babelCore.transform(code, {
       plugins: [
