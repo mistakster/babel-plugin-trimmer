7bc7d0dd4c885d517bada2340feea66b0f319e8e
Vladimir Kuznetsov <mistakster@gmail.com>
2017-01-18 10:27:48 +0500

    cut comments as well

--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
 const assert = require('assert');
 const babelCore = require('babel-core');
 
+function transform(code) {
+  const result = babelCore.transform(code, {
+    plugins: [
+      './lib/index.js'
+    ]
+  });
+
+  return result.code;
+}
+
 describe('babel-plugin-trimmer', () => {
   it('should trim all code after "trim-below" marker', () => {
     const code = `
@@ -20,48 +30,9 @@ function printTips() {
 function Tips() {
   return 321;
 }
-// comment-1
-// trim-below
-// comment-3`;
+// comment-1`;
 
-    const result = babelCore.transform(code, {
-      plugins: [
-        './lib/index.js'
-      ]
-    });
-
-    assert.strictEqual(result.code, expected);
+    assert.strictEqual(transform(code), expected);
   });
 
-  it('should trim all code before "trim-above" marker', () => {
-    const code = `
-function Tips() {
-  return 321;
-}
-// comment-1
-// trim-above
-// comment-3
-function printTips() {
-  return 123;
-}
-// comment-4
-`;
-
-    const expected = `
-// comment-1
-// trim-above
-// comment-3
-function printTips() {
-  return 123;
-}
-// comment-4`;
-
-    const result = babelCore.transform(code, {
-      plugins: [
-        './lib/index.js'
-      ]
-    });
-
-    assert.strictEqual(result.code, expected);
-  });
 });
