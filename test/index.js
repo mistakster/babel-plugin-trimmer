76c01ece156d1aeb0d398094460d0bc65c654b31
Vladimir Kuznetsov <mistakster@gmail.com>
2017-01-18 10:42:24 +0500

    added even more tests for trim-below marker

--- test/index.js
+++ test/index.js
@@ -31,6 +31,28 @@ function printTips() {
     assert.strictEqual(transform(code), expected);
   });
 
+  it.skip('should trim leave banner comment', () => {
+    const code = `
+/* banner */
+// trim-below
+function Tips() {
+  return 321;
+}
+// comment-1
+/* comment-2 */
+// comment-3
+function printTips() {
+  return 123;
+}
+// comment-4
+`;
+
+    const expected = `
+/* banner */`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
   it('should trim code and comments after "trim-below" marker', () => {
     const code = `
 function Tips() {
@@ -78,6 +100,91 @@ function Tips() {
 // comment-3
 function printTips() {
   return 123;
+}
+// comment-4`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it('should trim the code if "trim-below" marker is a block comment', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+/*
+
+trim-below
+
+*/
+// comment-3
+function printTips() {
+  return 123;
+}
+// comment-4
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it('should trim the code after "trim-below" marker inside block statement', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function printTips() {
+  var x = 0;
+  // comment-2
+  // trim-below
+  // comment-3
+  return x;
+}
+// comment-4
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function printTips() {
+  var x = 0;
+  // comment-2
+}
+// comment-4`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it.skip('should leave banner inside block statement', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function printTips() {
+  // banner
+  // trim-below
+  // comment-3
+  return x;
+}
+// comment-4
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function printTips() {
+  // banner
 }
 // comment-4`;
 
