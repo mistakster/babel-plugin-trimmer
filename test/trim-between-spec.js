b871c2392b5b12e9c47d9ed6bdb0d38a97c86548
Dmitry Nikulin <dmitry.nikulin@graph.uk>
2017-01-23 15:13:22 +0500

    added possibility to trim code between two markers

---
+++ test/trim-between-spec.js
@@ -0,0 +1,153 @@
+const assert = require('assert');
+const babelCore = require('babel-core');
+
+function transform(code) {
+  const result = babelCore.transform(code, {
+    plugins: [
+      './lib/index.js'
+    ]
+  });
+
+  return result.code;
+}
+
+describe('trim-between', () => {
+  it('should trim  all code if "trim-below" marker at the top and "trim-above" at the bottom ', () => {
+    const code = `// trim-below
+function Tips() {
+  return 321;
+}
+function someTips() {
+  return 2222;
+}
+// comment-1
+/* comment-2 */
+// comment-3
+function printTips() {
+  return 123;
+}
+// comment-4
+// trim-above
+`;
+
+    const expected = ``;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it('should trim code between "trim-below" and "trim-above "markers', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+// trim-below
+function someTips() {
+  return 2222;
+}
+// comment-3
+function printTips() {
+  return 123;
+}
+// trim-above
+// comment-4
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1
+
+// comment-4`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it('should trim code between "trim-below" and "trim-above "markers if "trim-below" marker at the top ', () => {
+    const code = `//trim-below
+function Tips() {
+  return 321;
+}
+// comment-1
+/* comment-2 */
+// comment-3
+function printTips() {
+  return 123;
+}
+// trim-above
+// comment-4
+function someTips() {
+  return 2222;
+}
+`;
+
+    const expected = `
+// comment-4
+function someTips() {
+  return 2222;
+}`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+
+  it('should trim code between "trim-below" and "trim-above "markers if "trim-above" marker at the bottom ', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+//trim-below
+function someTips() {
+  return 2222;
+}
+// comment-3
+function printTips() {
+  return 123;
+}
+// comment-4
+//trim-above
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+  it('should trim code between "trim-below" and "trim-above "markers if markers in only trailing ', () => {
+    const code = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function someTips() {
+  return 2222;
+}
+// comment-3
+function printTips() {
+  return 123;
+}
+//trim-below
+// comment-4
+//trim-above
+`;
+
+    const expected = `
+function Tips() {
+  return 321;
+}
+// comment-1
+function someTips() {
+  return 2222;
+}
+// comment-3
+function printTips() {
+  return 123;
+}`;
+
+    assert.strictEqual(transform(code), expected);
+  });
+});
